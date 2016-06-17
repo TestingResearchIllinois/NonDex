4057bbca6d81b3c19788c0d1839faa3e0f78a290
August Shi <awshi2@illinois.edu>
2016-06-17 17:49:24 -0500

    Modified printing of debugging with a newline

--- nondex-maven-plugin/src/main/java/edu/illinois/nondex/plugin/DebugMojo.java
+++ nondex-maven-plugin/src/main/java/edu/illinois/nondex/plugin/DebugMojo.java
@@ -80,7 +80,7 @@ public class DebugMojo extends AbstractNonDexMojo {
 
         this.getLog().warn("*********");
         for (String test : testToRepro.keySet()) {
-            this.getLog().warn("REPRO for " + test + ": mvn nondex:nondex " + testToRepro.get(test));
+            this.getLog().warn("REPRO for " + test + ":\nmvn nondex:nondex " + testToRepro.get(test));
         }
     }
 
