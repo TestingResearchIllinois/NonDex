d137875a7ee621652fd189c601ed89133a85fc68
Alex Gyori <gyori@illinois.edu>
2016-09-04 22:14:12 -0500

    more changes to maybe apease windows

--- nondex-maven-plugin/src/it/clean-it/verify.groovy
+++ nondex-maven-plugin/src/it/clean-it/verify.groovy
@@ -1,3 +1,5 @@
 File nondexDirectory = new File( basedir, ".nondex" );
 
+sleep(1000)
+
 assert ! nondexDirectory.exists();
--- nondex-maven-plugin/src/it/simple-it/verify.groovy
+++ nondex-maven-plugin/src/it/simple-it/verify.groovy
@@ -1,5 +1,7 @@
 File nondexDirectory = new File( basedir, ".nondex" );
 
+sleep(1000)
+
 assert nondexDirectory.isDirectory();
 
 assert nondexDirectory.list().length == 7
