9078c737df504c094e6bec73ec9b47d7db0fdd54
Alex Gyori <gyori@illinois.edu>
2016-10-07 13:48:22 -0500

    fixed debugging and typos and whatnot

--- nondex-common/src/main/java/edu/illinois/nondex/common/NonDex.java
+++ nondex-common/src/main/java/edu/illinois/nondex/common/NonDex.java
@@ -28,12 +28,17 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 package edu.illinois.nondex.common;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.logging.Level;
+
+import edu.illinois.nondex.shuffling.ControlNondeterminism;
 
 public class NonDex {
 
@@ -42,6 +47,8 @@ public class NonDex {
 
     private final Configuration config;
     private final Random        randomNumber;
+
+    private boolean isOutputting;
 
     public NonDex() {
         this(Configuration.parseArgs());
@@ -53,6 +60,8 @@ public class NonDex {
 
         this.config = config;
         this.randomNumber = new Random(config.seed);
+
+        this.isOutputting = false;
     }
 
     public boolean getBoolean() {
@@ -85,7 +94,7 @@ public class NonDex {
         return identityElement;
     }
 
-    public <T> List<T> permutation(List<T> originalOrder) {
+    public <T> List<T> getPermutation(List<T> originalOrder) {
 
         if (originalOrder == null) {
             throw new IllegalArgumentException("originalOrder is null");
@@ -117,8 +126,11 @@ public class NonDex {
 
     private boolean shouldExplore() {
         this.opportunityCount++;
-        if (this.shouldExploreForInstance() && this.apiShouldBeExplored()) {
+        // When outputting, it should not explore behaviors, since then we output some debug info
+        // and that should be ordered
+        if (!this.isOutputting && this.shouldExploreForInstance() && this.apiShouldBeExplored()) {
             Logger.getGlobal().log(Level.FINE, "Exploring for current source");
+            printStackTraceIfUniqueDebugPoint();
             this.actualCount++;
             return true;
         }
@@ -144,6 +156,39 @@ public class NonDex {
         return "UNKNOWN";
     }
 
+    private void printStackTraceIfUniqueDebugPoint() {
+        if (config.shouldPrintStackTrace
+                && this.isDebuggingUniquePoint()) {
+            StackTraceElement[] traces = Thread.currentThread().getStackTrace();
+            StringBuilder stackstring = new StringBuilder();
+            for (StackTraceElement traceElement : traces) {
+                stackstring.append(traceElement.toString() + String.format("%n"));
+            }
+            try {
+                // Writing to file invokes NonDex, so this flag is to prevent it from infinitely
+                // trying to write to file,
+                // and to prevent it from doing other things when all we want is to print out a
+                // stack trace
+                this.isOutputting = true;
+                Files.write(this.config.getDebugPath(),
+                        ("TEST: " + this.config.testName + String.format("%n")
+                                + stackstring.toString()).getBytes(),
+                        StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            } catch (IOException ioe) {
+                Logger.getGlobal().log(Level.SEVERE, "Exception when printing debug info.", ioe);
+            } finally {
+                this.isOutputting = false;
+            }
+        }
+    }
+
+    private boolean isDebuggingUniquePoint() {
+        return config.start >= 0 && config.end >= 0
+                && config.start == config.end
+                && this.getPossibleExplorations() == config.start;
+    }
+
+
     private boolean shouldExploreForInstance() {
         return this.opportunityCount >= this.config.start
                 && this.opportunityCount <= this.config.end;
@@ -153,7 +198,7 @@ public class NonDex {
         return this.config;
     }
 
-    public int getpossibleExplorations() {
+    public int getPossibleExplorations() {
         return this.opportunityCount;
     }
 
