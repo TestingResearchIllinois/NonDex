d9f4e40f3de0898f090f78e4affa84f7f385ff39
Can Wang <canwang2016@u.northwestern.edu>
2016-10-29 21:17:45 -0700

    Init Gradle plugin for NonDex

---
+++ nondex-gradle-plugin/src/main/groovy/edu/illinois/nondex/gradle/plugin/NonDexPlugin.groovy
@@ -0,0 +1,12 @@
+package edu.illinois.nondex.gradle.plugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+
+class NonDexPlugin implements Plugin<Project> {
+
+	@Override
+	void apply(Project project) {
+	}
+}
