da349bf37fbead6e7fb0079cae943012d15ba8c2
Dan Zhao <zpy1995@gmail.com>
2016-11-18 04:20:07 -0600

    remove trailing whitespace

--- nondex-instrumentation/src/main/java/edu/illinois/nondex/instr/PriorityQueueShufflingAdder.java
+++ nondex-instrumentation/src/main/java/edu/illinois/nondex/instr/PriorityQueueShufflingAdder.java
@@ -35,7 +35,7 @@ import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
 public class PriorityQueueShufflingAdder extends ClassVisitor {
-    
+
     public PriorityQueueShufflingAdder(ClassVisitor ca) {
         super(Opcodes.ASM5, ca);
     }
