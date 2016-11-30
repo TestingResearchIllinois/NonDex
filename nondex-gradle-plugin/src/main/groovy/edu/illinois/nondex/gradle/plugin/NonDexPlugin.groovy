163d652eeaff5ce37526c413f9c200f87da8e2ba
Can Wang <canwang2016@u.northwestern.edu>
2016-11-29 21:27:12 -0800

    Reformat the code in Gradle plugin

--- nondex-gradle-plugin/src/main/groovy/edu/illinois/nondex/gradle/plugin/NonDexPlugin.groovy
+++ nondex-gradle-plugin/src/main/groovy/edu/illinois/nondex/gradle/plugin/NonDexPlugin.groovy
@@ -1,10 +1,9 @@
 package edu.illinois.nondex.gradle.plugin
 
-import edu.illinois.nondex.gradle.plugin.tasks.NonDexHelp;
-import org.gradle.api.Plugin;
+import edu.illinois.nondex.gradle.plugin.tasks.NonDexHelp
+import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.JavaPlugin;
-
+import org.gradle.api.plugins.JavaPlugin
 
 class NonDexPlugin implements Plugin<Project> {
     static final String NONDEX_GROUP = 'nondex'
