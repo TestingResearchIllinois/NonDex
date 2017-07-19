1900c2cb9136ccd0fd6a035341f5804b2996f13a
Ben Lambeth <lambethben@gmail.com>
2017-07-18 20:41:20 -0700

    #85 fix (#113)
    * Tests now run in clean
    
    * fixed the issue with excluded groups, at least temporarily
    
    * hardening the test and asserting that some number of tests fail
    
    * eol
    
    * removing printStackTrace
    
    * Multi-module IT problem
    
    * fixing module issues
    
    * better logging
    
    * better logging
    
    * better logging
    
    * logging
    
    * Fixed excluded-groups-it bug and added some more tests
    
    * revert the refactoring as it is unrelated to the bug
    
    * Revert "revert the refactoring as it is unrelated to the bug"
    
    This reverts commit 7ec8cdee40378e2af88dcce09e1c42222f700f54.
    
    * Fixed tests and added one more test to ensure tests run
    
    * added gitignore info for emacs
    
    * appeasing some codacy

--- nondex-maven-plugin/src/it/comprehensive/verify.groovy
+++ nondex-maven-plugin/src/it/comprehensive/verify.groovy
@@ -4,3 +4,14 @@ assert nondexDirectory.isDirectory();
 
 // Every run adds 5 directories. Latest and jar are static
 assert (nondexDirectory.list().length - 2) % 5 == 0;
+
+def nondexFolder = new File(".nondex")
+dirs = nondexFolder.listFiles()
+
+for (File f : dirs) {
+    if (f.getName().startsWith("clean")) {
+       failures = new File(f.getAbsolutePath() + File.pathSeparator + "failures");
+       // in the clean phase all tests should fail, meaning at least 40
+       assert Files.readAllLines(failures.getPath(), Charset.defaultCharset()).size() > 40;
+    }
+}
---
+++ nondex-maven-plugin/src/it/excluded-groups-it/module2/src/test/java/edu/illinois/nondex/it/Module2Ignore.java
@@ -0,0 +1,3 @@
+package edu.illinois.nondex.it;
+
+public interface Module2Ignore {}
